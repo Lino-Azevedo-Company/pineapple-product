0d6832cdb72b5fae35f1ee016b4958bc5c9d3acf
Gabriel Souza <azevedoedit@gmail.com>
2022-07-20 22:39:53 -0300

    Add default create/drop database

---
+++ src/test/resources/com/pineapple/product/service/test/sql/create-database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE pna_product (
+    product_id      INTEGER AUTO_INCREMENT NOT NULL,
+    name            VARCHAR(200) NOT NULL,
+    descr           VARCHAR(4000),
+    descr_add       VARCHAR(4000),
+    model           VARCHAR(200) NOT NULL,
+    price           DECIMAL(10, 2) NOT NULL,
+    discount        DECIMAL(10, 2),
+    weight          DECIMAL(5),
+    dimensions      VARCHAR(8),
+    packing_weight  DECIMAL(5),
+    packing_dim     VARCHAR(8),
+    active          BOOLEAN
+);
+
+ALTER TABLE pna_product
+    ADD CONSTRAINT pna_product_pk PRIMARY KEY (product_id);
