adf2f0d94281dc499b3a2edbfb3f331011aed10b
Gabriel Souza <azevedoedit@gmail.com>
2022-07-20 22:38:54 -0300

    Fix test settings

--- src/test/java/com/pineapple/product/service/config/IntegrationTest.java
+++ src/test/java/com/pineapple/product/service/config/IntegrationTest.java
@@ -16,7 +16,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @ContextConfiguration(classes = {
-        JpaTestConfig.class
+        DatabaseTestConfig.class
 })
 public @interface IntegrationTest {
 }
