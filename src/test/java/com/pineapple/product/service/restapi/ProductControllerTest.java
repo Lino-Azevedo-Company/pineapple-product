8da212f19e8e9f7ae9af35cfd0803b59b85c7564
Gabriel Souza <azevedoedit@gmail.com>
2022-07-20 22:41:03 -0300

    Add ProductList Test

---
+++ src/test/java/com/pineapple/product/service/restapi/ProductControllerTest.java
@@ -0,0 +1,42 @@
+package com.pineapple.product.service.restapi;
+
+import com.pineapple.product.service.config.AbstractControllerTest;
+import com.pineapple.product.service.config.IntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@IntegrationTest
+public class ProductControllerTest extends AbstractControllerTest {
+
+    @Test
+    @DisplayName("Given A Request To List Products Should Return 200 And The List Of Products")
+    @Sql({
+            "/com/pineapple/product/service/test/sql/drop-database.sql",
+            "/com/pineapple/product/service/test/sql/create-database.sql"
+    })
+    @Sql(
+            "/com/pineapple/product/service/test/sql/products.sql"
+    )
+    void givenARequestToListProductsShouldReturn200AndTheListOfProducts() throws Exception {
+        var result = getMockMvc()
+                .perform(get("/products/"))
+                .andExpect(status().isOk())
+                .andReturn();
+        var json = getJsonArray(result);
+
+
+        JSONAssert.assertEquals(
+                jsonAsString("two-or-more-products.json"),
+                getResultAsJson(result),
+                true
+        );
+        assertThat(json.length(),equalTo(3));
+    }
+}
---
+++ src/test/resources/com/pineapple/product/service/test/sql/products.sql
@@ -0,0 +1,92 @@
+
+
+INSERT INTO pna_product
+    (
+        name,
+        descr,
+        descr_add,
+        model,
+        price,
+        discount,
+        weight,
+        dimensions,
+        packing_weight,
+        packing_dim,
+        active
+    )
+VALUES
+(
+     'Product 1',
+     'Descrição 1',
+     'Descrição adicional 1',
+     'Modelo 1',
+     11111.11,
+     11.11,
+     11.111,
+     '11x11x11',
+     11.111,
+     '11x11x11',
+     'true'
+);
+
+
+
+INSERT INTO pna_product
+    (
+        name,
+        descr,
+        descr_add,
+        model,
+        price,
+        discount,
+        weight,
+        dimensions,
+        packing_weight,
+        packing_dim,
+        active
+    )
+VALUES
+(
+     'Product 2',
+     'Descrição 2',
+     'Descrição adicional 2',
+     'Modelo 2',
+     11111.11,
+     11.11,
+     11.111,
+     '11x11x11',
+     11.111,
+     '11x11x11',
+     'true'
+);
+
+
+
+INSERT INTO pna_product
+    (
+        name,
+        descr,
+        descr_add,
+        model,
+        price,
+        discount,
+        weight,
+        dimensions,
+        packing_weight,
+        packing_dim,
+        active
+    )
+VALUES
+(
+     'Product 3',
+     'Descrição 3',
+     'Descrição adicional 3',
+     'Modelo 1',
+     11111.11,
+     11.11,
+     11.111,
+     '11x11x11',
+     11.111,
+     '11x11x11',
+     'true'
+);
