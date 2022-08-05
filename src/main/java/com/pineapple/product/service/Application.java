40c2577124d215daf15b13299435feb35d7035e5
TobiasLino <tobias.lino@sonda.com>
2022-08-05 14:20:52 -0300

    Add: Component scan for pineapple.commons package

--- src/main/java/com/pineapple/product/service/Application.java
+++ src/main/java/com/pineapple/product/service/Application.java
@@ -1,10 +1,11 @@
 package com.pineapple.product.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.pineapple.commons", "com.pineapple.product" })
 public class Application {
 
 	public static void main(String[] args) {
